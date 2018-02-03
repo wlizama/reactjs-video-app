b8411deb54288881b2e9d6c3e9166f781cca3393
Wilder Lizama <wilderlizama@gmail.com>
2018-02-02 23:52:16 -0500

    Dinamic props and PropTypes validation

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import Media from '../src/playlist/components/media'
 
 const app = document.getElementById("app");
 
-render(<Media />, app);
+render(<Media type="video" image="./images/covers/html5.jpg" title="Aprende HTML5" author="Yo mero" />, app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,4 +1,5 @@
 import React , {Component} from 'react';
+import PropTypes from 'prop-types';
 import './media.css'
 
 class Media extends Component {
@@ -6,13 +7,20 @@ class Media extends Component {
     return (
       <div className="Media">
         <div className="Media-cover">
-          <img src="./images/covers/bitcoin.jpg" alt="" className="Media-image" width={260} height={160} />
-          <h3 className="Media-title">tituliño</h3>
-          <p className="Media-author">autor</p>
+          <img className="Media-image"src={this.props.image} width={260} height={160} />
+          <h3 className="Media-title">{this.props.title}</h3>
+          <p className="Media-author">{this.props.author}</p>
         </div>
       </div>
     );
   }
+}
+
+Media.propTypes = {
+  image : PropTypes.string.isRequired,
+  title : PropTypes.string,
+  author : PropTypes.string,
+  type : PropTypes.oneOf(['video', 'audio'])
 }
 
 export default Media;
