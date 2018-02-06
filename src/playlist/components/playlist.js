3ca03cc0c3e012fc4ca0fdfb5da8275fb019bef1
Wilder Lizama <wilderlizama@gmail.com>
2018-02-05 22:19:39 -0500

    Icon Components composition

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Media from './media.js';
 import './playlist.css'
-
+import Play from '../../icons/components/play'
+import Pause from '../../icons/components/pause'
+import Volume from '../../icons/components/volume'
+import FullScreen from '../../icons/components/fullScreen'
 
 function Playlist(props) {
 
   const playlist = props.data.categories[1].playlist
-  console.log(props.data)
   
   return(
     <div className="Playlist">
+      <Play size={30} color="#8B1E95"/>
+      <Pause size={30} color="#406D83"/>
+      <Volume size={30} color="#E16716"/>
+      <FullScreen size={30} color="#303030"/>
       {
         playlist.map((item) => {
           return <Media key={item.id} {...item} />
