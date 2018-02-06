355048028f5e983421fe972691dfaa8f7badf86e
Wilder Lizama <wilderlizama@gmail.com>
2018-02-05 23:18:29 -0500

    Proyect structure, UI, components and containers

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    "reactjs-video-app": path.resolve(__dirname, 'src/index.js'),
+    "home": path.resolve(__dirname, 'src/entries/home.js'),
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
