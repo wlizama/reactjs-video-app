55e97e7d608e5de2bf1661308e28822d7ba0624a
Wilder Lizama <wilderlizama@gmail.com>
2018-02-06 21:52:16 -0500

    init left side layout

--- src/categories/components/category.js
+++ src/categories/components/category.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Playlist from '../../playlist/components/playlist'
+import './category.css'
 
 function Category(props) {
   return (
-    <div>
-      <p>{ props.description }</p>
-      <h2>{ props.title }</h2>
+    <div className="Category">
+      <p className="Category-title">{ props.description }</p>
+      <h2 className="Category-description">{ props.title }</h2>
       <Playlist playlist={props.playlist} />
     </div>
   )
