6e48a6aa090101816631ba81e5b22ac245c74ec2
Wilder Lizama <wilderlizama@gmail.com>
2018-02-21 22:09:50 -0500

    Volume element

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -7,6 +7,7 @@ import Timer from '../components/timer'
 import Controls from '../components/video-player-controls'
 import ProgressBar from '../components/progress-bar'
 import Spinner from '../components/spinner'
+import Volume from '../components/volume'
 
 class VideoPlayer extends Component {
   state = {
@@ -57,6 +58,10 @@ class VideoPlayer extends Component {
     })
   }
 
+  handleVolumeChange = evt => {
+    this.video.volume = evt.target.value
+  }
+
   render(){
     return(
       <VideoPlayerLayout>
@@ -65,6 +70,9 @@ class VideoPlayer extends Component {
           <PlayPause pause={this.state.pause} handleClick={this.tooglePlay} />
           <Timer duration={this.state.duration} currentTime={this.state.currentTime} />
           <ProgressBar duration={this.state.duration} value={this.state.currentTime} handleProgressChange={this.handleProgressChange} />
+          <Volume
+            handleVolumeChange={this.handleVolumeChange}
+          />
         </Controls>
         <Spinner
           active={this.state.loading}
